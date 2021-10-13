237b755dee282386d714e5aaa9bafaf136137978
lucy74310 <lucy74310@gmail.com>
2021-10-14 00:59:15 +0900

    async await homework

---
+++ async_ellie/async/promise-to-async-ellie.js
@@ -0,0 +1,42 @@
+class UserStorage {
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "ellie" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
+  }
+
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "ellie") {
+          resolve({ name: "ellie", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      }, 1000);
+    });
+  }
+  async getUserWithRole(user, password) {
+    const id = await this.loginUser(user, password);
+    const role = await this.getRoles(id);
+    return role;
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+
+userStorage
+  .getUserWithRole(id, password) //
+  .catch(console.log)
+  .then(console.log);
